c00ef4ca68a719f20d8dbe68bfe57ccd55902485
egormuratov <egor_muratov@hotmail.com>
2023-10-27 19:56:58 +0700

    initial commit 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,46 @@
 module.exports = {
   siteMetadata: {
     title: `blog`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://diversnotes.com/`,
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/locales`,
+        name: `locale`
+      }
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
+        languages: [`ru`, 'en'],
+        defaultLanguage: `ru`,
+        siteUrl: `https://diversnotes.com`,
+        // if you are using trailingSlash gatsby config include it here, as well (the default is 'always')
+        trailingSlash: 'always',
+        // you can pass any i18next options
+        i18nextOptions: {
+          interpolation: {
+            escapeValue: false // not needed for react as it escapes by default
+          },
+          debug: true,
+          keySeparator: false,
+          nsSeparator: false
+        },
+        pages: [
+          {
+            matchPath: '/:lang?/blog/:uid',
+            getLanguageFromPath: true,
+            excludeLanguages: ['ru', 'en']
+          },
+          {
+            matchPath: '/preview',
+            languages: ['ru']
+          }
+        ]
+      }
+    }
+  ],
 }
