0349330b21bbed2cef51422518ed18ac6fd1e7d8
egormuratov <egor_muratov@hotmail.com>
2023-11-04 18:13:41 +0700

    g-tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
       resolve: 'gatsby-plugin-react-i18next',
       options: {
         localeJsonSourceName: 'locale', // name given to `gatsby-source-filesystem` plugin.
-        languages: ['ru', 'en'],
+        languages: ['ru'],
         defaultLanguage: 'ru',
         siteUrl: 'https://diversnotes.com',
         // if you are using trailingSlash gatsby config include it here, as well (the default is 'always')
@@ -37,13 +37,31 @@ module.exports = {
           {
             matchPath: '/:lang?/blog/:uid',
             getLanguageFromPath: true,
-            excludeLanguages: ['ru', 'en']
+            excludeLanguages: ['ru']
           },
           {
             matchPath: '/preview',
             languages: ['ru']
           }
         ]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-5G8B88DZ4P' // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: []
+        }
       }
     }
   ]
