3a0fd7a2f41c403804fc46fe58b6c8c2fed75c2f
Muratov Egor <egor_muratov@hotmail.com>
2025-01-16 20:13:08 +0100

    initial commit

---
+++ app/[lang]/page.tsx
@@ -0,0 +1,16 @@
+import { getDictionary } from "../../get-dictionary";
+import { Locale } from "../../i18n-config";
+
+export default async function IndexPage(props: {
+  params: Promise<{ lang: Locale }>;
+}) {
+  const { lang } = await props.params;
+
+  const t = await getDictionary(lang);
+
+  return (
+    <div>
+      <h1>{t.title}</h1>
+    </div>
+  );
+}
