035e8f48865af1ff6881feed74933ab570e027c1
Muratov Egor <egor_muratov@hotmail.com>
2025-01-17 16:36:25 +0100

    Refactor middleware to include 'images' in the static file matcher; remove redundant static file check

--- middleware.ts
+++ middleware.ts
@@ -6,13 +6,6 @@ import { i18n } from "./i18n-config";
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
-  // Исключаем все статические файлы из перенаправления
-  const isStaticFile =  pathname.match(/^\/(.*\..*)$/);
-
-  if (isStaticFile) {
-    return NextResponse.next();
-  }
-
   const pathnameIsMissingLocale = i18n.locales.every(
     (locale) =>
       !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
@@ -29,5 +22,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images).*)"],
 };
