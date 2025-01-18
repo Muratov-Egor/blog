e9006105d0342cfe089a0652756f371424e08ed3
Muratov Egor <egor_muratov@hotmail.com>
2025-01-18 17:08:37 +0100

    Enhance blog page and Header component with localization support
    - Integrated localization by importing and utilizing the getDictionary function in both the blog page and Header component.
    - Updated the blog page header to display localized title and description, improving user experience for different languages.
    - Adjusted Header component styling for better spacing and layout consistency.

--- app/[lang]/blog/page.tsx
+++ app/[lang]/blog/page.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Locale } from "@/i18n-config";
 import { getBlogArticles } from "@/lib/blog";
+import { getDictionary } from "@/get-dictionary";
 
 export default async function IndexPage({
   params,
@@ -9,10 +10,14 @@ export default async function IndexPage({
 }) {
   const { lang } = await params;
   const articles = await getBlogArticles(lang);
+  const t = await getDictionary(lang);
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Blog</h1>
+      <div className="flex flex-col items-center border-b border-gray-200 pb-2 mb-10">
+        <h1 className="text-3xl font-bold mb-2">{t.blog.title}</h1>
+        <span className="text-sm text-gray-500 mb-6 text-center">{t.blog.description}</span>
+      </div>
       <ul className="space-y-4">
         {articles.map(article => (
           <li key={article.slug}>
