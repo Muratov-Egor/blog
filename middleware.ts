ffbdd6aab3ae4e6e31dc3fd2d3eb770c7d0b3bb7
Muratov Egor <egor_muratov@hotmail.com>
2025-01-19 17:02:48 +0100

    Implement middleware for database path redirection
    - Added logic to redirect requests starting with '/database' to a new path under '/marine-life'.
    - Updated the matcher configuration to include the new '/database/:path*' route for middleware processing.

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,13 @@ import { i18n } from "./i18n-config";
 export function middleware(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
+  // Проверяем, если путь начинается с /database
+  if (pathname.startsWith('/database')) {
+    const slug = pathname.replace('/database', ''); // Получаем slug
+    const newPath = `/marine-life${slug}`; // Формируем новый путь
+    return NextResponse.redirect(new URL(newPath, request.url)); // Перенаправляем на новый путь
+  }
+
   const pathnameIsMissingLocale = i18n.locales.every(
     (locale) =>
       !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`,
@@ -22,5 +29,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|images).*)"],
+  matcher: ["/database/:path*", "/((?!api|_next/static|_next/image|favicon.ico|images).*)"],
 };
