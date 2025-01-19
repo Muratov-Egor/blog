4822404c35394745ab97603302e246a76518c166
Muratov Egor <egor_muratov@hotmail.com>
2025-01-19 11:16:52 +0100

    Enhance Blog and Marine Life pages with localization and layout improvements
    - Updated BlogPage and MarineLifePage components to include localized titles and descriptions for better user experience.
    - Centered content layout for both pages to improve visual appeal and consistency.
    - Introduced dictionary fetching in MarineLifePage to support dynamic localization based on language selection.

--- app/[lang]/blog/page.tsx
+++ app/[lang]/blog/page.tsx
@@ -39,7 +39,7 @@ export default async function BlogPage({ params, searchParams }: Props) {
   const articles = allArticles.slice(startIndex, endIndex);
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8 text-center">
       <div className="flex flex-col items-center border-b border-gray-200 pb-2 mb-10">
         <h1 className="text-3xl font-bold mb-2">{t.blog.title}</h1>
         <span className="text-sm text-gray-500 mb-6 text-center">{t.blog.description}</span>
--- app/[lang]/marine-life/page.tsx
+++ app/[lang]/marine-life/page.tsx
@@ -36,6 +36,7 @@ export default async function MarineLifePage({
   const { lang } = await params;
   const { page } = await searchParams;
   const currentPage = Number(page) || 1;
+  const t = await getDictionary(lang);
   const articles = await getMarineLifeArticles(lang);
   const itemsPerPage = 9;
   const itemsPerFirstPage = 9;
@@ -49,8 +50,11 @@ export default async function MarineLifePage({
   const limitedArticles = articles.slice(startIndex, endIndex);
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Marine Life</h1>
+    <div className="container mx-auto px-4 py-8 text-center">
+      <div className="flex flex-col items-center border-b border-gray-200 pb-2 mb-10">
+        <h1 className="text-3xl font-bold mb-2">{t.marineLife.title}</h1>
+        <span className="text-sm text-gray-500 mb-6 text-center">{t.marineLife.description}</span>
+      </div>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {limitedArticles.map(article => (
           <MarineLifeCard 
