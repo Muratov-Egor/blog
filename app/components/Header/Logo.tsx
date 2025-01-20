47420e0d6ae9184992601492b6a8e21baa2c7bb4
Muratov Egor <egor_muratov@hotmail.com>
2025-01-20 10:01:18 +0100

    change h1 to p

--- app/components/Header/Logo.tsx
+++ app/components/Header/Logo.tsx
@@ -11,7 +11,7 @@ export default function Logo({ siteName, lang }: LogoProps) {
   return (
     <Link href={`/${lang}`} className="flex items-center gap-2 hover:no-underline flex-1">
       <Image src="/images/logo.png" alt="Logo" width="50" height="50" />
-      <h1 className="text-2xl font-bold whitespace-nowrap">{siteName}</h1>
+      <p className="text-3xl font-bold whitespace-nowrap">{siteName}</p>
     </Link>
   );
 }
