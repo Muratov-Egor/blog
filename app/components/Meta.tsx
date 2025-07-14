76ab159c955f7b434e63b78604a8dc0977aaf7e9
Muratov Egor <egor_muratov@hotmail.com>
2025-07-14 18:38:48 +0200

    site name

--- app/components/Meta.tsx
+++ app/components/Meta.tsx
@@ -5,13 +5,21 @@ import { Locale } from "@/i18n-config";
 type MetaProps = {
   params: Promise<{ lang: Locale }>;
   title: string;
+  siteName?: string;
   description: string;
   keywords?: string;
   openGraphImage?: string;
   canonicalUrl?: string;
 };
 
-export async function generateMeta({ params, title, description, keywords, openGraphImage, canonicalUrl }: MetaProps): Promise<Metadata> {
+export async function generateMeta({ params, title, description, keywords, openGraphImage, canonicalUrl }: MetaProps): Promise<{
+  title: string;
+  siteName: string;
+  description: string;
+  keywords: string;
+  openGraph: { title: string; description: string; images: string[]; siteName?: string; };
+  alternates: { canonical: string }
+}> {
   const { lang } = await params;
   const t = await getDictionary(lang);
   const baseUrl = `https://divernotes.com/${lang}`;
@@ -19,9 +27,11 @@ export async function generateMeta({ params, title, description, keywords, openG
 
   return {
     title: title || 'Diver\'s Notes',
+    siteName: 'Diver\'s Notes',
     description: description || 'Blog about scuba diving',
     keywords: keywords || "Scuba diving",
     openGraph: {
+      siteName: 'Diver\'s Notes',
       title: title || 'Diver\'s Notes',
       description: description || 'Blog about scuba diving',
       images: [openGraphImage || '/images/banner.png'],
@@ -30,4 +40,4 @@ export async function generateMeta({ params, title, description, keywords, openG
       canonical: canonical,
     },
   };
-} 
+}
